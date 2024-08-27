926ea4dfb0f21aa90d326dae0d07c9c1f4e2b4bd
NiiloOikari <Niilo.Oikari@gmail.com>
2024-08-27 13:59:04 +0300

    commit

---
+++ data.js
@@ -0,0 +1,471 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-aloha-ulkoa",
+      "name": "1 Aloha ulkoa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 1.5973688625937914,
+        "pitch": -0.359631273979506,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5611769733478678,
+          "pitch": -0.18849749094896495,
+          "rotation": 0,
+          "target": "1-2-aula"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5377001458836919,
+          "pitch": -0.39472203473908074,
+          "title": "Aloha tietotalo",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-2-aula",
+      "name": "2 Aula",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.296354630123968,
+        "pitch": 0.08019719435868211,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.312725525357507,
+          "pitch": 0.15790775529447387,
+          "rotation": 5.497787143782138,
+          "target": "6-kytv-"
+        },
+        {
+          "yaw": 1.7971107676691078,
+          "pitch": 0.135332920237337,
+          "rotation": 0,
+          "target": "7-21-hubi"
+        },
+        {
+          "yaw": 1.1566813647823384,
+          "pitch": 0.10414251786570006,
+          "rotation": 0.7853981633974483,
+          "target": "4-31-oikea-kaytava-paaty"
+        },
+        {
+          "yaw": -0.17116627657891037,
+          "pitch": 0.1413346998383922,
+          "rotation": 0,
+          "target": "5-vasen-kaytava"
+        },
+        {
+          "yaw": 2.9410433477141833,
+          "pitch": 0.09273068764779424,
+          "rotation": 0.7853981633974483,
+          "target": "0-1-aloha-ulkoa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7474835993283868,
+          "pitch": -0.14021771336498468,
+          "title": "Etätyötila",
+          "text": "Varattavissa"
+        }
+      ]
+    },
+    {
+      "id": "2-kahvihuone",
+      "name": "Kahvihuone",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7956210752464665,
+          "pitch": 0.12250982087642193,
+          "rotation": 0,
+          "target": "4-31-oikea-kaytava-paaty"
+        },
+        {
+          "yaw": 0.49889672593169365,
+          "pitch": 0.029875207874809462,
+          "rotation": 0,
+          "target": "9-keittio"
+        },
+        {
+          "yaw": -0.22259517160543751,
+          "pitch": 0.07797860865104056,
+          "rotation": 0,
+          "target": "8-4-oikea-takakaytava"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kopiointi",
+      "name": "Kopiointi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8340539429766665,
+          "pitch": 0.017093004650691412,
+          "rotation": 7.0685834705770345,
+          "target": "5-vasen-kaytava"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-31-oikea-kaytava-paaty",
+      "name": "3.1 Oikea käytävä pääty",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 1.178105553651692,
+        "pitch": 0.00443621939919403,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23869708702408232,
+          "pitch": 0.09587060134430203,
+          "rotation": 0,
+          "target": "6-kytv-"
+        },
+        {
+          "yaw": 1.670631725627306,
+          "pitch": 0.13824133054471055,
+          "rotation": 0,
+          "target": "2-kahvihuone"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-vasen-kaytava",
+      "name": "Vasen käytävä",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.757359320792675,
+        "pitch": -0.03300915313953645,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16990634418249684,
+          "pitch": 0.05894029533366307,
+          "rotation": 0.7853981633974483,
+          "target": "1-2-aula"
+        },
+        {
+          "yaw": 2.942811226423128,
+          "pitch": 0.05756007598445123,
+          "rotation": 0,
+          "target": "3-kopiointi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kytv-",
+      "name": "Käytävä ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 3.00663376722477,
+        "pitch": -0.08956844340802306,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.085673837814074,
+          "pitch": -0.003528676110551743,
+          "rotation": 0,
+          "target": "4-31-oikea-kaytava-paaty"
+        },
+        {
+          "yaw": 0.8131633233815858,
+          "pitch": -0.005924729323069755,
+          "rotation": 0,
+          "target": "1-2-aula"
+        },
+        {
+          "yaw": -0.16443169769649302,
+          "pitch": 0.0131558731236332,
+          "rotation": 0,
+          "target": "0-1-aloha-ulkoa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-21-hubi",
+      "name": "2.1 Hubi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 1.6327981479395897,
+        "pitch": 0.15024188900123292,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6197391007353632,
+          "pitch": 0.07831326372708425,
+          "rotation": 0,
+          "target": "1-2-aula"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-4-oikea-takakaytava",
+      "name": "4. Oikea takakäytävä",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1298746901623034,
+          "pitch": -0.007852366434438096,
+          "rotation": 0,
+          "target": "2-kahvihuone"
+        },
+        {
+          "yaw": -0.4079717219275203,
+          "pitch": -0.02282452978615268,
+          "rotation": 0,
+          "target": "1-2-aula"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-keittio",
+      "name": "Keittiö",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -2.0040513064687797,
+        "pitch": 0.15968782539301252,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6240554496492408,
+          "pitch": 0.12542719099603694,
+          "rotation": 0,
+          "target": "2-kahvihuone"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Aloha karvalakkikierros",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
